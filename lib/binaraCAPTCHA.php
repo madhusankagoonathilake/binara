433dec22f6dee7f3a9871dc2f1075813e1983b02
madhusanka.goonathilake <madhusanka.goonathilake@gmail.com>
2011-09-12 11:10:30 +0000

    Added noise generation; Added a background "stem" to run through the word; Changed the background color to white;

--- lib/binaraCAPTCHA.php
+++ lib/binaraCAPTCHA.php
@@ -39,28 +39,47 @@ class binaraCAPTCHA {
 
         $image = imagecreatetruecolor($this->config->get('width'), $this->config->get('height'));
         $white = imagecolorallocate($image, 255, 255, 255);
-        $pink = imagecolorallocate($image, 255, 200, 200);
         $black = imagecolorallocate($image, 0, 0, 0);
 
         $fonts = $this->getFontPaths();
 
         $maxFontIndex = count($fonts) - 1;
 
-        imagefill($image, 0, 0, $pink);
+        imagefill($image, 0, 0, $white);
 
         $x = rand(10, 20);
+
+        $prevMidRightX = null;
+        $prevMidRightY = null;
+
         foreach ($chars as $i => $char) {
-            $size = rand(12, 30);
-            $angle = rand(10, 30) * ((rand(100, 999) % 2 == 1) ? 1 : -1);
+            $size = rand(20, 30);
+            $angle = rand(0, 20) * ((rand(100, 999) % 2 == 1) ? 1 : -1);
             $font = $fonts[rand(0, $maxFontIndex)];
 
             $boundBox = imageftbbox($size, $angle, $font, $char);
 
-            $x += ($boundBox[2] - $boundBox[0]) + rand(3, 4);
+            $x += ($boundBox[2] - $boundBox[0]) + rand(7,8);
             $y = 50 + floor(($boundBox[7] - $boundBox[1]) / 3);
+
+            $midLeftX = $x + floor(($boundBox[6] + $boundBox[0]) / 8);
+            $midLeftY = $y + floor(($boundBox[7] + $boundBox[1]) / 4);
+            $midRightX = $x + floor(($boundBox[4] + $boundBox[2]) / 8);
+            $midRightY = $y + floor(($boundBox[5] + $boundBox[3]) / 4);
+
+            binaraImageHelper::instance()->addThickLine($image, $midLeftX, $midLeftY, $midRightX, $midRightY, $black, 3);
+
+            if (!is_null($prevMidRightX)) {
+                binaraImageHelper::instance()->addThickLine($image, $prevMidRightX, $prevMidRightY, $midLeftX, $midLeftY, $black, 3);
+            }
+
+            $prevMidRightX = $midRightX;
+            $prevMidRightY = $midRightY;
 
             imagefttext($image, $size, $angle, $x, $y, $black, $font, $char);
         }
+        
+        binaraImageHelper::instance()->addNoise($image);
 
         $this->image = $image;
     }

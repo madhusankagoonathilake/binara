654f1c6813f46dc1930deb193e032c4d1a6d729f
madhusanka.goonathilake <madhusanka.goonathilake@gmail.com>
2012-07-21 05:16:40 +0000

    Added binaraException class

---
+++ lib/binaraException.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * binara ver 1.0
+ * http://code.google.com/p/binara/
+ * 
+ * Copyright (c) 2011 Madhusanka Goonathilake
+ * 
+ * Licensed under the MIT licenses.
+ * http://code.google.com/p/binara/wiki/License
+ */
+class binaraException extends Exception {
+    
+}
